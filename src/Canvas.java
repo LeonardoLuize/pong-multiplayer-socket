09b4365c30b8b3fe986f0e54d5cd90c8fc122c8d
danielnowakassis <daniel.nowak.assis@gmail.com>
2021-11-05 21:58:30 -0300

    Canvas

---
+++ src/Canvas.java
@@ -0,0 +1,52 @@
+
+import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+class Surface extends JPanel {
+
+    private void doDrawing(Graphics g) {
+
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.drawString("Java 2D", 50, 50);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+        doDrawing(g);
+    }
+}
+
+public class Canvas extends JFrame {
+
+    public Canvas() {
+
+        initUI();
+    }
+
+    private void initUI() {
+
+        add(new Surface());
+
+        setTitle("Simple Java 2D example");
+        setSize(300, 200);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+                Canvas ex = new Canvas();
+                ex.setVisible(true);
+            }
+        });
+    }
+}
